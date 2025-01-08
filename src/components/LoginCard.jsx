0e5d68f61b3bd0cba30a840af7e84557584827d1
Soumik Sen <mastersoumik2003@gmail.com>
2025-01-08 13:53:27 +0530

    Update LoginCard.jsx

--- src/components/LoginCard.jsx
+++ src/components/LoginCard.jsx
@@ -39,7 +39,7 @@ const LoginCard = () => {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ email, password }),
-          { withCredentials: true},
+          credentials: "include",
         }
       );
 
